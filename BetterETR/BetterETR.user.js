e2f5416bd788be59d35ede87b42861dcb390235a
Mátyás Mustoha <mmatyas@inf.u-szeged.hu>
2015-01-24 12:38:34 +0100

    [BetterETR] Manuális újraszámolás gomb.

--- BetterETR/BetterETR.user.js
+++ BetterETR/BetterETR.user.js
@@ -7,20 +7,37 @@
 // ==/UserScript==
 
 //
+// Util
+//
+
+function insertAfter(newNode, referenceNode) {
+    referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+}
+
+//
 // Összesített kredit és óraszám
 //
 
-var kurzus_tabla = document.getElementsByClassName("kurz_ul")[0];
-var kurzusok = kurzus_tabla.children;
+function osszesites() {
+    var kurzus_tabla = document.getElementsByClassName("kurz_ul")[0];
+    var kurzusok = kurzus_tabla.children;
 
-var kredit = 0;
-var oraszam = 0;
-for (var k = 0; k < kurzusok.length; k++) {
-    var kurzus_str = kurzusok[k].children[0].rows[0].cells[3].innerHTML.split(" ");
-    kredit += parseInt(kurzus_str[0]);
-    oraszam += parseInt(kurzus_str[3]);
+    var kredit = 0;
+    var oraszam = 0;
+    for (var k = 0; k < kurzusok.length; k++) {
+        var kurzus_str = kurzusok[k].children[0].rows[0].cells[3].innerHTML.split(" ");
+        kredit += parseInt(kurzus_str[0]);
+        oraszam += parseInt(kurzus_str[3]);
+    }
+
+    var osszesitett_str = document.createElement("div");
+    osszesitett_str.innerHTML = kredit + " kr / " + oraszam + " óra";
+    kurzus_tabla.parentNode.appendChild(osszesitett_str);
 }
 
-var osszesitett_str = document.createElement("div");
-osszesitett_str.innerHTML = kredit + " kr / " + oraszam + " óra";
-kurzus_tabla.parentNode.appendChild(osszesitett_str);
+var ciklusvalaszto = document.getElementById("hallg_kurzus_lista_fillter_div_ciklus");
+var calcBtn = document.createElement("button");
+calcBtn.appendChild(document.createTextNode("Óraszám számolás"));
+calcBtn.onclick = osszesites;
+insertAfter(calcBtn, ciklusvalaszto);
+osszesites();
